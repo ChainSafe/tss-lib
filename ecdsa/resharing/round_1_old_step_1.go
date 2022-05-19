e56c6d901cae4b3c60b55ba7f165ea89de79cf91
Matija Petrunic <matija.petrunic@fer.hr>
2022-05-19 12:36:39 +0200

    Ignore processing messages that were already processed in resharing round 1

--- ecdsa/resharing/round_1_old_step_1.go
+++ ecdsa/resharing/round_1_old_step_1.go
@@ -93,6 +93,10 @@ func (round *round1) Update() (bool, *tss.Error) {
 	}
 	// accept messages from old -> new committee
 	for j, msg := range round.temp.dgRound1Messages {
+		if round.oldOK[j] {
+			continue
+		}
+
 		if msg == nil || !round.CanAccept(msg) {
 			return false, nil
 		}
