dc55f932e3009b723719429768bd29db5e9f86d0
Timofey <timofey.luin@gmail.com>
2022-12-16 14:22:33 +0200

    Add input validation to Dlog proofs (#6)
    * Validate discrete log proof
    
    * Add checks to Feldman VSS
    
    * Cherry pick changes from bnb-chain/tss-lib

--- crypto/vss/feldman_vss.go
+++ crypto/vss/feldman_vss.go
@@ -11,6 +11,7 @@
 package vss
 
 import (
+	"crypto/elliptic"
 	"errors"
 	"fmt"
 	"math/big"
@@ -39,15 +40,35 @@ var (
 	one  = big.NewInt(1)
 )
 
+// Check share ids of Shamir's Secret Sharing, return error if duplicate or 0 value found
+func CheckIndexes(ec elliptic.Curve, indexes []*big.Int) ([]*big.Int, error) {
+	visited := make(map[string]struct{})
+	for _, v := range indexes {
+		vMod := new(big.Int).Mod(v, ec.Params().N)
+		if vMod.Cmp(zero) == 0 {
+			return nil, errors.New("party index should not be 0")
+		}
+		vModStr := vMod.String()
+		if _, ok := visited[vModStr]; ok {
+			return nil, fmt.Errorf("duplicate indexes %s", vModStr)
+		}
+		visited[vModStr] = struct{}{}
+	}
+	return indexes, nil
+}
+
 // Returns a new array of secret shares created by Shamir's Secret Sharing Algorithm,
 // requiring a minimum number of shares to recreate, of length shares, from the input secret
-//
 func Create(threshold int, secret *big.Int, indexes []*big.Int) (Vs, Shares, error) {
 	if secret == nil || indexes == nil {
 		return nil, nil, fmt.Errorf("vss secret or indexes == nil: %v %v", secret, indexes)
 	}
 	if threshold < 1 {
 		return nil, nil, errors.New("vss threshold < 1")
+	}
+	ids, err := CheckIndexes(tss.EC(), indexes)
+	if err != nil {
+		return nil, nil, err
 	}
 	num := len(indexes)
 	if num < threshold {
@@ -63,11 +84,8 @@ func Create(threshold int, secret *big.Int, indexes []*big.Int) (Vs, Shares, err
 
 	shares := make(Shares, num)
 	for i := 0; i < num; i++ {
-		if indexes[i].Cmp(big.NewInt(0)) == 0 {
-			return nil, nil, fmt.Errorf("party index should not be 0")
-		}
-		share := evaluatePolynomial(threshold, poly, indexes[i])
-		shares[i] = &Share{Threshold: threshold, ID: indexes[i], Share: share}
+		share := evaluatePolynomial(threshold, poly, ids[i])
+		shares[i] = &Share{Threshold: threshold, ID: ids[i], Share: share}
 	}
 	return v, shares, nil
 }
@@ -138,8 +156,8 @@ func samplePolynomial(threshold int, secret *big.Int) []*big.Int {
 
 // Evauluates a polynomial with coefficients such that:
 // evaluatePolynomial([a, b, c, d], x):
-// 		returns a + bx + cx^2 + dx^3
 //
+//	returns a + bx + cx^2 + dx^3
 func evaluatePolynomial(threshold int, v []*big.Int, id *big.Int) (result *big.Int) {
 	q := tss.EC().Params().N
 	modQ := common.ModInt(q)
